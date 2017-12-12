b917b0e37ae870e22b70f83b1f63e72cfa53fa39
Garrett Missiaen <missiaen@uwm.edu>
2017-12-12 12:12:23 -0600

    Supported the ability to add keyframes easily and merged with Matts changes

--- cs459final.cpp
+++ cs459final.cpp
@@ -1,6 +1,6 @@
-// Project 3
+// Final Project
 // CS459
-// By Garrett Missiaen
+// By Matthew Buchanan, Philip Kocol, and Garrett Missiaen
 // readOFF code taken from Lab 8
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -20,7 +20,7 @@ static float alpha = 0.0;
 static float beta = PI / 6.0;
 static GLdouble cpos[3];
 static GLint angleView = 30.0;
-bool flag = true;//used for color change
+bool flag = true, pause = false;//used for color change
 
 static GLfloat lpos[] = { -1.0, 2.0, 1.0, 1.0 };
 static GLfloat white[] = { 1.0, 1.0, 1.0, 1.0 };
@@ -38,60 +38,75 @@ static GLfloat **cdata;//array for polygon color
 bool fullscreen = false;
 bool mouseDown = false;
 
-float xscale = 1.0;
-float yscale = 1.0;
-float zscale = 1.0;
-float xrot = 0.0f;
-float yrot = 0.0f;
-float zrot = 0.0f;
-float xtrans = 0;
-float ytrans = 0;
-float ztrans = 0;
+float currentframe[3][3] = {
+	{0.0, 0.0, 0.0},
+	{0.0, 0.0, 0.0},
+	{1.0, 1.0, 1.0}
+};
 int reflect = 1;
 
 int refreshTimer = 15; //time between refreshes in milliseconds
-int numIntPoints = 700; //number of points to interpolate
-
-float frames[2][3][3] = {
-	{ { -1,1,-1 },{ -100,-100,-100 },{ 0.9,0.9,0.9 } },
-	{ { 1,-1,0 },{ 300,200,100 },{ 1.0,2,2 } }
-};
+int numIntPoints = 200; //number of points to interpolate
 int currentIntPoint = 0;
+int numFrames = 2;
+int currIndex = 0;
+int nextIndex = 1;
+
+#define maxframes 20
+
+//1st index frames number
+//2nd index translation, rotation, scale
+//3rd index x, y, z
+float frames[maxframes][3][3] = {
+	{ {-1,1,-1}, {-100,-100,-100}, {0.9,0.9,0.9} },
+	{ {1,-1,0}, {300,200,100}, {1.0,2,2} }
+};
+float zoom = 5.0;
+
 
 bool both = false;
 
 #define min(x,y) ((x)<(y) ? (x) : (y))
 #define max(x,y) ((x)>(y) ? (x) : (y))
 
+
+void writemessage()
+{
+	printf("\n\
+	Final Project by Matthew Buchanan, Philip Kocol, and Garrett Missiaen\n\
+	Allows defining keyframs and animates between them\n\
+	a ------------------------- add keyframe\n\
+	l ------------------------- change color\n\
+	-, + ---------------------- change zoom\n\
+	c ------------------------- change off files\n\
+	x, X, y, Y, z, Z ---------- change light source position\n\
+	spacebar ------------------ pause/unpause\n\
+	F1 ------------------------ fullscreen\n\
+	ESC ----------------------- exit\n\
+	\n");
+}
+
+
 void readOFF()//method to read .off format data. Borrowed partially form Dr. Zeyun Yu homework
 			  //modified further to read polygons of any size
 			  //nonconvex and nonflat polygons may not be rendered correctly
 {
-	FILE* fin;
-	char filename[FILENAME_MAX];
-
-	fin = NULL;
-	printf("\n\nEnter an .off file name including extension: ");
-	while (fin == NULL) {
-		int result = scanf("%99[^\n]%*c", &filename);
-		fin = fopen(filename, "rb");
-	};
-
-	int res = 0;
-	bool goodNum = false;
-	printf("\n\nEnter the number of interpolation points(must be greater than 0): ");
-	while (!goodNum) {
-		res = scanf("%d", &numIntPoints);
-		if (res >= 0) {
-			goodNum = true;
-		}
-	};
-
 	int n, j;
 	int a, b;
 	float x, y, z;
 	float resize;
 	char line[256];
+
+	FILE* fin;
+	char filename[FILENAME_MAX];
+
+	fin = NULL;
+	while (fin == NULL) {
+		printf("\n\nEnter an .off file name including extension (or press Enter to abort): ");
+		int result = scanf("%99[^\n]%*c", filename);
+		if (result != 1) exit(0);
+		fin = fopen(filename, "rb");
+	};
 
 	/* OFF format */
 	while (fgets(line, 256, fin) != NULL) {
@@ -141,6 +156,19 @@ void readOFF()//method to read .off format data. Borrowed partially form Dr. Zey
 	fclose(fin);
 	printf("done.\n");
 
+}
+
+void setInterpolationPoints()
+{
+	int res = 0;
+	bool goodNum = false;
+	printf("\n\nEnter the number of interpolation points(must be greater than 0): ");
+	while (!goodNum) {
+		res = scanf("%d", &numIntPoints);
+		if (res >= 0) {
+			goodNum = true;
+		}
+	}
 }
 
 void calculateNormal()//calculates the normal vector for every polygon
@@ -196,7 +224,7 @@ void reshape(int w, int h)
 	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(angleView, (GLfloat)w / (GLfloat)h, 1.5, 20.0);
+	gluPerspective(angleView, (GLfloat)w / (GLfloat)h, 1.5, 40.0);
 	glMatrixMode(GL_MODELVIEW);
 }
 
@@ -206,7 +234,7 @@ void display(void)
 
 	cpos[0] = 5.0 * cos(beta) * sin(alpha);
 	cpos[1] = 5.0 * sin(beta);
-	cpos[2] = 5.0 * cos(beta) * cos(alpha);
+	cpos[2] = zoom * cos(beta) * cos(alpha);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	gluLookAt(cpos[0], cpos[1], cpos[2], 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
@@ -228,11 +256,11 @@ void display(void)
 	glPushMatrix();
 
 	//Transform based on mouse input
-	glTranslatef(xtrans, ytrans, ztrans);
-	glRotatef(xrot, 1.0f, 0.0f, 0.0f);
-	glRotatef(yrot, 0.0f, 1.0f, 0.0f);
-	glRotatef(zrot, 0.0f, 0.0f, 1.0f);
-	glScalef(xscale, yscale, zscale);
+	glTranslatef(currentframe[0][0], currentframe[0][1], currentframe[0][2]);
+	glRotatef(currentframe[1][0], 1.0f, 0.0f, 0.0f);
+	glRotatef(currentframe[1][1], 0.0f, 1.0f, 0.0f);
+	glRotatef(currentframe[1][2], 0.0f, 0.0f, 1.0f);
+	glScalef(currentframe[2][0], currentframe[2][1], currentframe[2][2]);
 
 
 	//Loop through each face we are drawing
@@ -271,6 +299,78 @@ void display(void)
 	glutSwapBuffers();
 }
 
+void addKeyframe()
+{
+	//TODO: Add a keyframe from user input right here
+
+	//Translation
+	frames[numFrames][0][0] = rand() / RAND_MAX;
+	frames[numFrames][0][1] = rand() / RAND_MAX;
+	frames[numFrames][0][2] = rand() / RAND_MAX;
+	//Rotation
+	frames[numFrames][1][0] = rand() / (RAND_MAX / 100);
+	frames[numFrames][1][1] = rand() / (RAND_MAX / 100);
+	frames[numFrames][1][2] = rand() / (RAND_MAX / 100);
+	//Scaling
+	frames[numFrames][2][0] = 1;
+	frames[numFrames][2][1] = 1;
+	frames[numFrames][2][2] = 1;
+	numFrames++; if (numFrames >= maxframes) numFrames = maxframes - 1;
+
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	switch (key) {
+	case 27:
+		exit(0);
+		break;
+	case 'c':   // change off file        
+		printf("change off file...\n");
+		readOFF();
+		setRandomColor();
+		calculateNormal();
+		break;
+	case 'x':
+		lpos[0] = lpos[0] + 0.2;
+		break;
+	case 'X':
+		lpos[0] = lpos[0] - 0.2;
+		break;
+	case 'y':
+		lpos[1] = lpos[1] + 0.2;
+		break;
+	case 'Y':
+		lpos[1] = lpos[1] - 0.2;
+		break;
+	case 'z':
+		lpos[2] = lpos[2] + 0.2;
+		break;
+	case 'Z':
+		lpos[2] = lpos[2] - 0.2;
+		break;
+	case 'l':
+		flag = !flag;
+		break;
+	case ' ':
+		pause = !pause;
+		break;
+	case '+':
+		zoom -= 1.0;
+		if (zoom < 1) zoom = 1;
+		break;
+	case '-':
+		zoom += 1.0;
+		if (zoom > 30) zoom = 30;
+		break;
+	case 'a':
+		addKeyframe();
+	default:
+		break;
+	}
+	glutPostRedisplay;
+}
+
 void specialkey(GLint key, int x, int y)
 {
 	switch (key) {
@@ -289,55 +389,104 @@ void specialkey(GLint key, int x, int y)
 	}
 }
 
+void menu(int value) {
+	switch (value) {
+	case 0:
+		exit(0);
+		break;
+	case 1:
+		glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
+		both = false;
+		break;
+	case 2:
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		both = false;
+		break;
+	case 3:
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		both = false;
+		break;
+	case 4:
+		glPolygonMode(GL_BACK, GL_FILL);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		both = true;
+		break;
+	default:
+		break;
+	}
+	glutPostRedisplay();
+}
+
+void createmenu(void) {
+
+	glutCreateMenu(menu);
+
+	// Create an entry
+	glutAddMenuEntry("Point Mode", 1);
+	glutAddMenuEntry("Line Mode", 2);
+	glutAddMenuEntry("Fill Mode", 3);
+	glutAddMenuEntry("Both Mode", 4);
+	glutAddMenuEntry("Exit", 0);
+
+	// Let the menu respond on the right mouse button
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+}
 
 void setTranslation() {
 
-	float xtransFactor = (frames[1][0][0] - frames[0][0][0]) / numIntPoints;
-	float ytransFactor = (frames[1][0][1] - frames[0][0][1]) / numIntPoints;
-	float ztransFactor = (frames[1][0][2] - frames[0][0][2]) / numIntPoints;
+	float xtransFactor = (frames[nextIndex][0][0] - frames[currIndex][0][0]) / numIntPoints;
+	float ytransFactor = (frames[nextIndex][0][1] - frames[currIndex][0][1]) / numIntPoints;
+	float ztransFactor = (frames[nextIndex][0][2] - frames[currIndex][0][2]) / numIntPoints;
 
-	xtrans = frames[0][0][0] + currentIntPoint * xtransFactor;
-	ytrans = frames[0][0][1] + currentIntPoint * ytransFactor;
-	ztrans = frames[0][0][2] + currentIntPoint * ztransFactor;
+	currentframe[0][0] = frames[currIndex][0][0] + currentIntPoint * xtransFactor;
+	currentframe[0][1] = frames[currIndex][0][1] + currentIntPoint * ytransFactor;
+	currentframe[0][2] = frames[currIndex][0][2] + currentIntPoint * ztransFactor;
 }
 
 void setRotation() {
 
-	float xrotFactor = (frames[1][1][0] - frames[0][1][0]) / numIntPoints;
-	float yrotFactor = (frames[1][1][1] - frames[0][1][1]) / numIntPoints;
-	float zrotFactor = (frames[1][1][2] - frames[0][1][2]) / numIntPoints;
+	float xrotFactor = (frames[nextIndex][1][0] - frames[currIndex][1][0]) / numIntPoints;
+	float yrotFactor = (frames[nextIndex][1][1] - frames[currIndex][1][1]) / numIntPoints;
+	float zrotFactor = (frames[nextIndex][1][2] - frames[currIndex][1][2]) / numIntPoints;
 
-	xrot = frames[0][1][0] + currentIntPoint * xrotFactor;
-	yrot = frames[0][1][1] + currentIntPoint * yrotFactor;
-	zrot = frames[0][1][2] + currentIntPoint * zrotFactor;
+	currentframe[1][0] = frames[currIndex][1][0] + currentIntPoint * xrotFactor;
+	currentframe[1][1] = frames[currIndex][1][1] + currentIntPoint * yrotFactor;
+	currentframe[1][2] = frames[currIndex][1][2] + currentIntPoint * zrotFactor;
 }
 
 void setScale() {
 
-	float xscaleFactor = (frames[1][2][0] - frames[0][2][0]) / numIntPoints;
-	float yscaleFactor = (frames[1][2][1] - frames[0][2][1]) / numIntPoints;
-	float zscaleFactor = (frames[1][2][2] - frames[0][2][2]) / numIntPoints;
+	float xscaleFactor = (frames[nextIndex][2][0] - frames[currIndex][2][0]) / numIntPoints;
+	float yscaleFactor = (frames[nextIndex][2][1] - frames[currIndex][2][1]) / numIntPoints;
+	float zscaleFactor = (frames[nextIndex][2][2] - frames[currIndex][2][2]) / numIntPoints;
 
-	xscale = frames[0][2][0] + currentIntPoint * xscaleFactor;
-	yscale = frames[0][2][1] + currentIntPoint * yscaleFactor;
-	zscale = frames[0][2][2] + currentIntPoint * zscaleFactor;
+	currentframe[2][0] = frames[currIndex][2][0] + currentIntPoint * xscaleFactor;
+	currentframe[2][1] = frames[currIndex][2][1] + currentIntPoint * yscaleFactor;
+	currentframe[2][2] = frames[currIndex][2][2] + currentIntPoint * zscaleFactor;
 }
 
 void timer(int value) {
 
-	setTranslation();
-	setRotation();
-	setScale();
+	if (!pause) {
+		setTranslation();
+		setRotation();
+		setScale();
 
-	currentIntPoint += reflect;
-	if (currentIntPoint >= numIntPoints || currentIntPoint <= 0) { reflect *= -1; }
+		currentIntPoint = (currentIntPoint + 1) % numIntPoints;
+		if (currentIntPoint >= numIntPoints || currentIntPoint <= 0) { 
+			currIndex = (currIndex + 1) % numFrames; 
+			nextIndex = (currIndex + 1) % numFrames;
+		}
 
-	glutPostRedisplay();      // Post re-paint request to activate display()
+		glutPostRedisplay();      // Post re-paint request to activate display()
+	}
+
 	glutTimerFunc(refreshTimer, timer, 0);
 }
 
 int main(int argc, char** argv)
 {
+	writemessage();
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(1200, 800);
@@ -354,12 +503,17 @@ int main(int argc, char** argv)
 
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
+	glutKeyboardFunc(keyboard);
 	glutSpecialFunc(specialkey);
 	glutTimerFunc(refreshTimer, timer, 0); // next timer call milliseconds later
+	createmenu();
 
 	readOFF();
 	calculateNormal();
 	setRandomColor();
+	setInterpolationPoints();
 	glutMainLoop();
 	return 0;
 }
+
+
